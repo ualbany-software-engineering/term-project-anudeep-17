1c13cef20972b37ae5940fdb86c485e28a533904
anudeep-17 <anudeepsai88@gmail.com>
2022-11-22 21:10:26 -0500

    update checks

--- client/src/profiles/editprofile.js
+++ client/src/profiles/editprofile.js
@@ -1,26 +1,52 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Axios from "axios";
 import {useNavigate} from "react-router-dom";
 
 export const Editprofile = () => {
 
     const navigate = useNavigate();
+    const [photo, setPhoto] = useState("");
+    const [name, setName] = useState("");
+    const [description, setDescription] = useState("");
+    const [photo1, setPhoto1] = useState("");
+    const [name1, setName1] = useState("");
+    const [description1, setDescription1] = useState("");
+    
     const edituser = () => {
-        Axios.post("http://localhost:3005/editprofile", {
-            id: "637affe160ed422ceccf0e0a",
-            photo: "https://c.ndtvimg.com/2020-07/m7opt04g_ms-dhoni-afp_625x300_06_July_20.jpg",
-        }).then((response) => {
+        Axios.post("http://localhost:3005/edit", {_id:"637afabb60ed422ceccf0da5",photo, name, description}).then((response) => {
             console.log("edited")
         });
-        // navigate("/")
+        navigate("/")
       };
+      
 
-    useEffect(()=>{
-     edituser()   
-    })
+      useEffect(() => {
+        
+        Axios.get("http://localhost:3005/getprofile").then((response) => {
+          for(let i = 0; i< response.data.length; i++)
+          {
+            if(response.data[i]._id.includes("637afabb60ed422ceccf0da5"))
+            {
+              setName1(response.data[i].name);
+              setPhoto1(response.data[i].photo);
+              setDescription1(response.data[i].description);
+            }
+          }
+        })
+      }, []);
+
   return (
     <div>
-        hi 
+    <div className='container form'> <>
+    <h1>edit Posts to Profile</h1>
+    <input type="text" placeholder={photo1} onChange={(e) => setPhoto(e.target.value) }/>
+    <input type="text" placeholder={name1} onChange={(e) => setName(e.target.value) }/>
+    <input type="text" placeholder={description1} onChange={(e) => setDescription(e.target.value) }/>
+
+    <button className='btn btn-primary' onClick = {edituser}>edit</button>
+    <button className='btn btn-primary' onClick = {()=>{navigate("/")}}>cancel</button>
+    </>
+    </div>
     </div>
   )
 }
